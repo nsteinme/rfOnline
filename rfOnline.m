1904321d7453108e60068a357c7d12fcd2ea610f
nsteinme <nick.steinmetz@gmail.com>
2015-09-09 17:37:42 +0100

    update main function with outline

--- rfOnline.m
+++ rfOnline.m
@@ -2,6 +2,10 @@
 
 function bestCoords = rfOnline(filename)
 % Compute a receptive field map online for sparse noise stimuli. 
+% Uses an estimate of the MUA signal (raw data, high pass filtered) for a
+% subset of channels. 
+% Produces a plot of the RF location for these channels and a suggested
+% coordinate to use for the center. 
 
 
 % read file metadata
@@ -12,4 +16,39 @@ function bestCoords = rfOnline(filename)
 % load Protocol
 
 
-% load synch channel from 
+% load synch channel from dat file and detect pulses (assume that should
+% use the most recent two)
+
+% synchronize TL and dat
+
+
+% extract trial times from photodiode
+
+
+% recreate stimulus traces
+
+
+% load each dat channel to analyze
+for ch = 1:numChansToAnalyze
+    
+    % load data
+    dat = readDatLimited(filename, nChans, analyzeChans(ch));
+    
+    % filter for MUA, smooth, then downsample
+    mua = datToMUA(dat, Fs, newFs);
+    
+    
+    % compute spike-trig averages
+    
+    % extract magnitude of responses
+    
+    % find peak
+    
+    % plot
+    
+end
+
+% plot summary
+
+% compute best coordinates
+   
