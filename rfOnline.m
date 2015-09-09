389b77a567c958bcf6a2293fb2b3bbf26731203c
nsteinme <nick.steinmetz@gmail.com>
2015-09-09 16:38:59 +0100

    added main function

---
+++ rfOnline.m
@@ -0,0 +1,15 @@
+
+
+function bestCoords = rfOnline(filename)
+% Compute a receptive field map online for sparse noise stimuli. 
+
+
+% read file metadata
+
+
+% load Timeline
+
+% load Protocol
+
+
+% load synch channel from 
